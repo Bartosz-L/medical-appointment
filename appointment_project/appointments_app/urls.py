2bade4d5d1d9e87a33f468b67624a388e7529360
Bartosz Ligęza <bligeza@tidk.pl>
2019-05-09 16:02:51 +0200

    added registration template and urls

--- appointment_project/appointments_app/urls.py
+++ appointment_project/appointments_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'appointments_app'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('registration/', views.registration, name='registration'),
+    path('registration/create', views.createPatientLogIn, name='createPatientLogIn'),
     path('verify/', views.verify, name="verify"),
 ]
